4456ede6046eeafd1c519160d09704161014716e
frustrated018 <niloyd018@gmail.com>
2023-10-09 01:46:40 +0600

    Added a slider to the home page using react carousel minimal

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import Slider from "../../Components/Slider/Slider";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="text-3xl text-center text-yellow-400">this is the home page</div>
-            <Slider></Slider>
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-3xl text-center text-yellow-400">
+        this is the home page
+      </div>
+      <div className=" mx-3 md:mx-6 lg:mx-0">
+        <Slider></Slider>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
